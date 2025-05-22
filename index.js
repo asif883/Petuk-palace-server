f367df108c84e4d995b83ab590d551a9a64273a0
asif883 <asifiqbal95863@gmail.com>
2025-05-22 12:54:55 +0600

    verfiy user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -15,6 +16,36 @@ app.use(cors({
     optionsSuccessStatus: 200
   }))
 app.use(express.json())
+
+
+// token verification 
+const verifyJwt =(req, res , next) =>{
+    const authorization = req.headers.authorization
+    if( !authorization ){
+      return res.send({ message: "No token found"})
+    }
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token , process.env.ACCESS_TOKEN, (err, decoded) =>{
+      if(err){
+        return res.send({message : "Invalid token"})
+      }
+      req.decoded= decoded;
+      next()
+    })
+  }
+
+ // verify Admin
+
+ const verifyAdmin = async ( req ,res ,next)=>{
+    const email = req.decoded.email
+    const query = { email : email}
+    const user = await userCollection.findOne(query)
+    if(user?.role !== 'admin'){
+      return res.send({message: "Forbidden access"})
+    }
+    next()
+   }
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.osztyuf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -51,7 +82,7 @@ const dbConnect = async() =>{
         res.send(result)
     })
     
-    app.get('/users' , async( req, res ) =>{
+    app.get('/users' ,verifyJwt, verifyAdmin, async( req, res ) =>{
         const allUser = await userCollection.find().toArray()
         res.send(allUser)
     })
@@ -119,18 +150,24 @@ const dbConnect = async() =>{
     })
    
     // add menu
-    app.post('/add-menu', async (req, res) => {
+    app.post('/add-menu',verifyJwt,verifyAdmin, async (req, res) => {
     const newItem = req.body;
     const result = await menuCollection.insertOne(newItem);
     res.send(result);
     });
 
-    app.get('/menu', async( req, res )=> {
+    app.get('/menu',verifyAdmin, async( req, res )=> {
         const menu = await menuCollection.find().toArray()
         res.send(menu)
     })
 
-
+    // jwt 
+    app.post('/authentication', async ( req , res )=>{
+    const userEmail = req.body;
+    const token = jwt.sign(userEmail , process.env.ACCESS_TOKEN, 
+      { expiresIn: '1d'})
+      res.send({token})
+   })
 
 
     }
